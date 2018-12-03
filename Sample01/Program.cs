0e72267bccc5975fb87e08de1ddffc28e4309410
dyb2018 <858452946@qq.com>
2018-12-03 15:09:01 +0800

    Update Program.cs

--- Sample01/Program.cs
+++ Sample01/Program.cs
@@ -12,13 +12,29 @@ namespace Sample01
 {
     public class Program
     {
+        /// <summary>
+        /// Main方法，程序的入口方法
+        /// </summary>
+        /// <param name="args"></param>
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)//调用下面的方法，返回一个IWebHostBuilder对象
+                .Build()//用上面返回的IWebHostBuilder对象创建一个IWebHost
+                .Run();//运行上面创建的IWebHost对象从而运行我们的Web应用程序换句话说就是启动一个一直运行监听http请求的任务
         }
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)//使用默认的配置信息来初始化一个新的IWebHostBuilder实例
+        //        .UseStartup<Startup>();// 为Web Host指定了Startup类
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureAppConfiguration((hostingContent, config) =>
+            {
+                var env = hostingContent.HostingEnvironment;
+
+                config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json")
+            });
     }
 }
