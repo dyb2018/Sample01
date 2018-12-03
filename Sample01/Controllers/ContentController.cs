a074dd7fb451979ab4e29a8e18ab0cfd9645389f
dyb2018 <858452946@qq.com>
2018-12-03 14:51:51 +0800

    Update ContentController.cs

--- Sample01/Controllers/ContentController.cs
+++ Sample01/Controllers/ContentController.cs
@@ -9,18 +9,26 @@ namespace Sample01.Controllers
 {
     public class ContentController : Controller
     {
+        private readonly Content contents;
+        public ContentController(Microsoft.Extensions.Options.IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
         /// <summary>
         /// 首页显示
         /// </summary>
         /// <returns></returns>
         public IActionResult Index()
         {
-            var contents = new List<Content>();
-            for (int i = 1; i < 11; i++)
-            {
-                contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
-            }
-            return View(new ContentViewModel { Contents = contents });
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i) });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
         }
     }
 }
